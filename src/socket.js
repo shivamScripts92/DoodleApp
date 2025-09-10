9ce0473d1882b9440503243d07fc645f9603c894
Shivam Krishna Srivastava <129311162+shivamScripts92@users.noreply.github.com>
2025-09-10 17:13:10 +0530

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,8 @@
 import { io } from "socket.io-client";
-const URL = process.env.NODE_ENV === 'production' ? 'https://jscafe-sketchbook-server.onrender.com' : 'http://localhost:5000'
+
+=
+const URL = process.env.NODE_ENV === 'production' 
+  ? 'https://doodleserver-4t78.onrender.com' // Render server URL
+  : 'http://localhost:5000';
+
 export const socket = io(URL);
