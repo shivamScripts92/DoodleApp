75405d160a58b3a51b96be8653ef4e67806421b7
Shivam Krishna Srivastava <129311162+shivamScripts92@users.noreply.github.com>
2025-09-10 17:18:55 +0530

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,5 @@
 import { io } from "socket.io-client";
 
-=
 const URL = process.env.NODE_ENV === 'production' 
   ? 'https://doodleserver-4t78.onrender.com' // Render server URL
   : 'http://localhost:5000';
